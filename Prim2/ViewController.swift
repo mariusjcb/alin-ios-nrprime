2f1e03a4c7fd0c575e1d7b204f724cf92573a64b
Marius Ilie <marius_ilie20082001@yahoo.com>
2017-01-08 15:05:33 +0200

    ToDo: Verifica nr. prim la apasrea unei taste

--- Prim2/ViewController.swift
+++ Prim2/ViewController.swift
@@ -9,17 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var number: Int? {
+        didSet {
+            if number != nil {
+                textField.text = String(describing: number!)
+                isPrime = check(prime: number)
+            } else {
+                textField.text = nil
+                statusLabel.text = "PRIM APP"
+                view.backgroundColor = UIColor.white
+            }
+        }
+    }
+    
+    var isPrime = false {
+        didSet {
+            if isPrime == false {
+                statusLabel.text = "NEPRIM"
+                view.backgroundColor = UIColor.red
+            } else {
+                statusLabel.text = "PRIM"
+                view.backgroundColor = UIColor.green
+            }
+        }
+    }
+    
+    func check(prime number: Int!) -> Bool {
+        if number <= 1 {
+            return false
+        } else if number <= 3{
+            return true
+        }
+        
+        for i in 2...(number-1) {
+            if number % i == 0 {
+                return false
+            }
+        }
+            
+        return true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var statusLabel: UILabel!
+
+    @IBOutlet weak var textField: UITextField!
+    
+    func hideKeyboard() {
+        textField.resignFirstResponder()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(
+            target: self,
+            action: #selector(hideKeyboard))
+        )
     }
 
-
+    @IBAction func onButtonTap(_ sender: UIButton) {
+        if let textFieldContent = textField.text {
+            number = Int(textFieldContent)
+        }
+    }
+    
+    @IBAction func changeNumber(_ sender: UIButton) {
+        if let currentTitle = sender.currentTitle {
+            number = Int(currentTitle)
+        }
+    }
 }
 
